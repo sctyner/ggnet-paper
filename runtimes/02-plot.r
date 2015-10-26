35ae36e83552cc8069bec0cd5dcbad02f99fce8b
François <briatte@gmail.com>
2015-10-26 20:38:48 +0100

    runtimes: better plot + results 180-200

--- runtimes/02-plot.r
+++ runtimes/02-plot.r
@@ -4,13 +4,15 @@ library(dplyr)
 
 d = lapply(list.files(pattern = "csv$"),
            read.csv, stringsAsFactors = FALSE)
+
 d = do.call(rbind, d)
 
-ggplot(gather(d, key, value, -network_size, -iteration) %>%
-         group_by(network_size, key) %>%
-         summarise(value = mean(value)),
-       aes(x = network_size, y = value, linetype = key)) +
+ggplot(gather(d, `Visualization method`, time, -network_size, -iteration) %>%
+         group_by(network_size, `Visualization method`) %>%
+         summarise(mean_time = mean(time)),
+       aes(x = network_size, y = mean_time, linetype = `Visualization method`)) +
   geom_line() +
   geom_point(size = 1) +
   labs(y = "Average plotting time (seconds)", x = "Network size") +
-  theme_classic()
+  theme_classic() +
+  theme(legend.justification = c(0,1), legend.position = c(0,1))
